4591351f9e2a9e6fbc95247f7c94b56842540cf7
Danylo <114859769+Almixs@users.noreply.github.com>
2024-03-29 09:21:07 +0200

    laba7a completed

---
+++ laba_7a/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 1;
+    printf("n=%d \n",n);
+    
+    // n++;
+    printf("prefix:   ++n=%d\n",++n);
+    printf("postfix:  n++=%d\n",n++);
+    printf("after-postfix: n=%d\n",n);
+    
+    // n--;
+    printf("prefix:   --n=%d\n",--n);
+    printf("postfix:  n--=%d\n",n--);
+    printf("after-postfix: n=%d\n",n);
+    return 0;
+}
